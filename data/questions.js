f1ec0328d3e2d58d4ebabf5c4726ec0762c864e6
Milad Mehri <memm.milad@gmail.com>
2024-09-21 13:35:29 -0700

    Add questions

--- data/questions.js
+++ data/questions.js
@@ -1,6 +1,21 @@
-[
+let questions = [
   {
-    "content": "// Alan is designing a spaceship dashboard where the flight coordinates are updated continuously. He needs to know if a specific number exists in the flight coordinates array.\n// Write a function `searchCoordinates(coordinates: number[], target: number): boolean` that returns true if the target exists in the array, otherwise false.\n// Example 1:\n// Input: coordinates = [1, 3, 5, 7, 9], target = 5\n// Output: true\n// Example 2:\n// Input: coordinates = [2, 4, 6, 8, 10], target = 11\n// Output: false",
+    "content": `// Alan is designing a spaceship dashboard where the flight
+// coordinates are updated continuously. He needs to know if a
+// specific number exists in the flight coordinates array.
+
+// Write a function
+// \`searchCoordinates(coordinates: number[], target: number):
+// boolean\` that returns true if the target exists in the array,
+// otherwise false.
+
+// Example 1:
+// Input: coordinates = [1, 3, 5, 7, 9], target = 5
+// Output: true
+
+// Example 2:
+// Input: coordinates = [2, 4, 6, 8, 10], target = 11
+// Output: false`,
     "testcase": [
       { "coordinates": [1, 3, 5, 7, 9], "target": 5 },
       { "coordinates": [2, 4, 6, 8, 10], "target": 11 },
@@ -14,7 +29,22 @@
     "output_type": "boolean"
   },
   {
-    "content": "// Alvin is writing a system to track all the devices he's lost. He needs a function that returns the index of the first occurrence of the target gadget in a list, or -1 if it's not found.\n// Write a function `findGadgetIndex(gadgets: string[], target: string): number` that returns the index of the target.\n// Example 1:\n// Input: gadgets = ['Phone', 'Tablet', 'Laptop'], target = 'Tablet'\n// Output: 1\n// Example 2:\n// Input: gadgets = ['Headphones', 'Phone', 'Smartwatch'], target = 'Laptop'\n// Output: -1",
+    "content": `// Alvin is writing a system to track all the devices he's
+// lost. He needs a function that returns the index of the first
+// occurrence of the target gadget in a list, or -1 if it's not
+// found.
+
+// Write a function
+// \`findGadgetIndex(gadgets: string[], target: string): number\`
+// that returns the index of the target.
+
+// Example 1:
+// Input: gadgets = ['Phone', 'Tablet', 'Laptop'], target = 'Tablet'
+// Output: 1
+
+// Example 2:
+// Input: gadgets = ['Headphones', 'Phone', 'Smartwatch'], target = 'Laptop'
+// Output: -1`,
     "testcase": [
       { "gadgets": ["Phone", "Tablet", "Laptop"], "target": "Tablet" },
       { "gadgets": ["Headphones", "Phone", "Smartwatch"], "target": "Laptop" },
@@ -28,7 +58,21 @@
     "output_type": "number"
   },
   {
-    "content": "// Milad is overwhelmed with the number of DMs he's receiving. He needs a function that removes duplicate messages from an array.\n// Write a function `removeDuplicates(messages: string[]): string[]` that returns the list without duplicates.\n// Example 1:\n// Input: messages = ['Hi', 'Hello', 'Hi', 'Hey']\n// Output: ['Hi', 'Hello', 'Hey']\n// Example 2:\n// Input: messages = ['Good morning', 'Good morning', 'Good night']\n// Output: ['Good morning', 'Good night']",
+    "content": `// Milad is overwhelmed with the number of DMs he's receiving.
+// He needs a function that removes duplicate messages from an
+// array.
+
+// Write a function
+// \`removeDuplicates(messages: string[]): string[]\` that returns
+// the list without duplicates.
+
+// Example 1:
+// Input: messages = ['Hi', 'Hello', 'Hi', 'Hey']
+// Output: ['Hi', 'Hello', 'Hey']
+
+// Example 2:
+// Input: messages = ['Good morning', 'Good morning', 'Good night']
+// Output: ['Good morning', 'Good night']`,
     "testcase": [
       { "messages": ["Hi", "Hello", "Hi", "Hey"] },
       { "messages": ["Good morning", "Good morning", "Good night"] },
@@ -49,7 +93,21 @@
     "output_type": "string[]"
   },
   {
-    "content": "// Austin is always causing chaos on the bus by sitting in the middle, making everyone around him leave. He needs a function to remove the elements next to him.\n// Write a function `removeNeighbors(seats: string[], index: number): string[]` that removes the elements adjacent to Austin's seat.\n// Example 1:\n// Input: seats = ['Person', 'Austin', 'Person'], index = 1\n// Output: ['Empty', 'Austin', 'Empty']\n// Example 2:\n// Input: seats = ['Person', 'Person', 'Austin', 'Person', 'Person'], index = 2\n// Output: ['Person', 'Empty', 'Austin', 'Empty', 'Person']",
+    "content": `// Austin is always causing chaos on the bus by sitting in the
+// middle, making everyone around him leave. He needs a function
+// to remove the elements next to him.
+
+// Write a function
+// \`removeNeighbors(seats: string[], index: number): string[]\`
+// that removes the elements adjacent to Austin's seat.
+
+// Example 1:
+// Input: seats = ['Person', 'Austin', 'Person'], index = 1
+// Output: ['Empty', 'Austin', 'Empty']
+
+// Example 2:
+// Input: seats = ['Person', 'Person', 'Austin', 'Person', 'Person'], index = 2
+// Output: ['Person', 'Empty', 'Austin', 'Empty', 'Person']`,
     "testcase": [
       { "seats": ["Person", "Austin", "Person"], "index": 1 },
       {
@@ -73,7 +131,20 @@
     "output_type": "string[]"
   },
   {
-    "content": "// Alan is building a storage system for the spaceship. He needs to sort all items based on their weight.\n// Write a function `sortItemsByWeight(items: number[]): number[]` that sorts the array of item weights in ascending order.\n// Example 1:\n// Input: items = [10, 5, 8, 2, 4]\n// Output: [2, 4, 5, 8, 10]\n// Example 2:\n// Input: items = [100, 50, 75, 20]\n// Output: [20, 50, 75, 100]",
+    "content": `// Alan is building a storage system for the spaceship. He needs
+// to sort all items based on their weight.
+
+// Write a function
+// \`sortItemsByWeight(items: number[]): number[]\` that sorts the
+// array of item weights in ascending order.
+
+// Example 1:
+// Input: items = [10, 5, 8, 2, 4]
+// Output: [2, 4, 5, 8, 10]
+
+// Example 2:
+// Input: items = [100, 50, 75, 20]
+// Output: [20, 50, 75, 100]`,
     "testcase": [
       { "items": [10, 5, 8, 2, 4] },
       { "items": [100, 50, 75, 20] },
@@ -94,7 +165,21 @@
     "output_type": "number[]"
   },
   {
-    "content": "// Alvin loves collecting gadgets, but he often misplaces them. He needs a function that counts how many times each gadget appears in his collection.\n// Write a function `countGadgets(gadgets: string[]): Record<string, number>` that returns a map of gadgets and their count.\n// Example 1:\n// Input: gadgets = ['Phone', 'Tablet', 'Phone', 'Laptop']\n// Output: { Phone: 2, Tablet: 1, Laptop: 1 }\n// Example 2:\n// Input: gadgets = ['Watch', 'Phone', 'Phone', 'Watch']\n// Output: { Watch: 2, Phone: 2 }",
+    "content": `// Alvin loves collecting gadgets, but he often misplaces them.
+// He needs a function that counts how many times each gadget
+// appears in his collection.
+
+// Write a function
+// \`countGadgets(gadgets: string[]): Record<string, number>\`
+// that returns a map of gadgets and their count.
+
+// Example 1:
+// Input: gadgets = ['Phone', 'Tablet', 'Phone', 'Laptop']
+// Output: { Phone: 2, Tablet: 1, Laptop: 1 }
+
+// Example 2:
+// Input: gadgets = ['Watch', 'Phone', 'Phone', 'Watch']
+// Output: { Watch: 2, Phone: 2 }`,
     "testcase": [
       { "gadgets": ["Phone", "Tablet", "Phone", "Laptop"] },
       { "gadgets": ["Watch", "Phone", "Phone", "Watch"] },
@@ -115,7 +200,21 @@
     "output_type": "Record<string, number>"
   },
   {
-    "content": "// Milad is so popular that people are constantly sending him questions. He needs a function to return only the DMs that end with a question mark.\n// Write a function `filterQuestions(messages: string[]): string[]` that filters the array to return only questions.\n// Example 1:\n// Input: messages = ['How are you?', 'I’ll be there', 'What time is it?']\n// Output: ['How are you?', 'What time is it?']\n// Example 2:\n// Input: messages = ['Are you coming?', 'Sure', 'Where are you?']\n// Output: ['Are you coming?', 'Where are you?']",
+    "content": `// Milad is so popular that people are constantly sending him
+// questions. He needs a function to return only the DMs that end
+// with a question mark.
+
+// Write a function
+// \`filterQuestions(messages: string[]): string[]\` that filters
+// the array to return only questions.
+
+// Example 1:
+// Input: messages = ['How are you?', 'I’ll be there', 'What time is it?']
+// Output: ['How are you?', 'What time is it?']
+
+// Example 2:
+// Input: messages = ['Are you coming?', 'Sure', 'Where are you?']
+// Output: ['Are you coming?', 'Where are you?']`,
     "testcase": [
       { "messages": ["How are you?", "I’ll be there", "What time is it?"] },
       { "messages": ["Are you coming?", "Sure", "Where are you?"] },
@@ -136,7 +235,20 @@
     "output_type": "string[]"
   },
   {
-    "content": "// Austin tends to disrupt people when sitting on a bus, so he needs to ensure the seats are empty around him. Write a function `refillSeats(seats: string[], index: number): string[]` that removes and refills the seats adjacent to Austin.\n// Example 1:\n// Input: seats = ['Empty', 'Austin', 'Empty'], index = 1\n// Output: ['Person', 'Austin', 'Person']\n// Example 2:\n// Input: seats = ['Empty', 'Austin', 'Empty', 'Empty'], index = 1\n// Output: ['Person', 'Austin', 'Person', 'Empty']",
+    "content": `// Austin tends to disrupt people when sitting on a bus, so he
+// needs to ensure the seats are empty around him.
+
+// Write a function
+// \`refillSeats(seats: string[], index: number): string[]\` that
+// removes and refills the seats adjacent to Austin.
+
+// Example 1:
+// Input: seats = ['Empty', 'Austin', 'Empty'], index = 1
+// Output: ['Person', 'Austin', 'Person']
+
+// Example 2:
+// Input: seats = ['Empty', 'Austin', 'Empty', 'Empty'], index = 1
+// Output: ['Person', 'Austin', 'Person', 'Empty']`,
     "testcase": [
       { "seats": ["Empty", "Austin", "Empty"], "index": 1 },
       { "seats": ["Empty", "Austin", "Empty", "Empty"], "index": 1 },
@@ -156,4 +268,6 @@
     "input_type": { "seats": "string[]", "index": "number" },
     "output_type": "string[]"
   }
-]
+];
+
+export default questions;
