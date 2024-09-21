90650d8806f075b680e2d9a09057cd865c47fa6f
alan <alcapone@students.cs.ubc.ca>
2024-09-21 10:49:13 -0700

    added resizable area

--- app/page.js
+++ app/page.js
@@ -1,12 +1,33 @@
 import { Button } from "@/components/ui/button";
 import CodeEditor from "@/components/ui/CodeEditor";
+import TextToSpeech from "@/components/ui/text_to_speech";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-background">
-      <CodeEditor width="60vw" /> 
+    <div className="">
+      <ResizablePanelGroup direction="horizontal" className="w-full">
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-full items-center justify-center p-6">
+            <CodeEditor />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-full items-center justify-center p-6">
+            <TooltipProvider>
+              <TextToSpeech />
+            </TooltipProvider>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
